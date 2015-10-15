442712747d5bf3c1a082edb087c1b9f6b727149d
Blank <shacknetisp@mail.com>
2015-10-15 19:32:14 -0400

    lock db

--- lib/db.py
+++ lib/db.py
@@ -13,28 +13,29 @@ modified = {}
 class DB:
 
     def __init__(self, path, d=None, userdata=True):
-        if d is None:
-            d = {}
-        path = ("%s/%s" % (bot.userdata, path)) if userdata else path
-        if path not in dbs:
-            if os.path.exists(path):
-                try:
-                    dbs[path] = json.load(open(path))
-                except ValueError as e:
-                    print(("(%s) Invalid JSON, loading with AST." % path))
+        with lock:
+            if d is None:
+                d = {}
+            path = ("%s/%s" % (bot.userdata, path)) if userdata else path
+            if path not in dbs:
+                if os.path.exists(path):
                     try:
-                        dbs[path] = ast.literal_eval(open(path).read())
-                    except SyntaxError as e:
-                        raise SyntaxError(
-                            "Invalid AST (%s): %s" % (path, e))
+                        dbs[path] = json.load(open(path))
                     except ValueError as e:
-                        raise ValueError(
-                            "Invalid AST (%s): %s" % (path, e))
-            else:
-                dbs[path] = d
-        self.d = dbs[path]
-        self.path = path
-        modified[self.path] = True
+                        print(("(%s) Invalid JSON, loading with AST." % path))
+                        try:
+                            dbs[path] = ast.literal_eval(open(path).read())
+                        except SyntaxError as e:
+                            raise SyntaxError(
+                                "Invalid AST (%s): %s" % (path, e))
+                        except ValueError as e:
+                            raise ValueError(
+                                "Invalid AST (%s): %s" % (path, e))
+                else:
+                    dbs[path] = d
+            self.d = dbs[path]
+            self.path = path
+            modified[self.path] = True
 
     def save(self):
         with lock:
