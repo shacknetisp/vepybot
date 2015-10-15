bb3e106e759c2aebf05e0b07aef713f4f4d2d195
Blank <shacknetisp@mail.com>
2015-10-15 17:18:38 -0400

    v0.1.2

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from threading import Lock
 threads = True
 run = True
 
-version = "0.1.1"
+version = "0.1.2"
 versionname = "Vepybot"
 versionstring = "%s %s" % (versionname, version)
 versiontuple = (versionname, version, "Python %s on %s" % (
