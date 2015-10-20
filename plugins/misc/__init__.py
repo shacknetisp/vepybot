4971906483e841fc5150b8acd46f86fb8950ec0a
Blank <shacknetisp@mail.com>
2015-10-20 11:06:55 -0400

    restructure lookup modules

--- plugins/misc/__init__.py
+++ plugins/misc/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Various utility commands.
 import bot
-from . import calc, chatbot, define
-[bot.reload(x) for x in [calc, chatbot, define]]
+from . import calc, chatbot, lookup
+[bot.reload(x) for x in [calc, chatbot, lookup]]
--- plugins/misc/lookup.py
+++ plugins/misc/lookup.py
@@ -7,9 +7,13 @@ tag_re = re.compile(r'<.*?>.*?<\/.*?>')
 
 class Module(bot.Module):
 
-    index = "ddg"
+    index = 'lookup'
 
     def register(self):
+
+        self.addcommand(self.ud, 'ud',
+            'Look up a term in Urban Dictionary.', ['term...'])
+
         self.addcommand(self.ddg, "ddg",
             "Look up something on Duck Duck Go. "
             "http://duckduckgo.com",
@@ -43,5 +47,38 @@ class Module(bot.Module):
         elif r['Redirect']:
             return "%s" % r['Redirect']
         return "No results."
+
+    def ud(self, context, args):
+        http = self.server.rget('http.url')
+        term = args.getstr('term')
+        try:
+            r = http.request('http://api.urbandictionary.com/v0/define',
+                params={'term': term}, timeout=4).json()
+        except http.Error:
+            return "Unable to contact the urbandictionary.com api."
+        except ValueError:
+            return "Invalid response from the urbandictionary.com api."
+        possible = []
+        first = ""
+        final = ""
+        for entry in r['list']:
+            definition = entry['definition']
+            if not first:
+                first = definition
+            if entry['example']:
+                definition += " | %s" % entry['example']
+            if entry['thumbs_up'] / max(entry['thumbs_down'], 1) > 5:
+                possible.append(definition)
+        if not possible:
+            final = first
+        else:
+            for p in sorted(possible, key=lambda x: len(x)):
+                if len(p) < self.server.opt('charlimit') - 40:
+                    final = p
+                    break
+            if not final:
+                final = first
+        final = ' '.join(final.split())
+        return final or "No definition found."
 
 bot.register.module(Module)
