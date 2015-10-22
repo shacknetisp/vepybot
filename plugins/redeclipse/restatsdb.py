9e62d1c871495760287cab9b5f54176479828fab
Blank <shacknetisp@mail.com>
2015-10-22 07:52:29 -0400

    0.2.6

--- plugins/redeclipse/restatsdb.py
+++ plugins/redeclipse/restatsdb.py
@@ -11,7 +11,7 @@ class Module(bot.Module):
     def register(self):
         self.addcommand(self.main, 'get',
             'Query a statsdb-interface API.'
-            ' Actions: toprace <map>',
+            ' Actions: toprace <map>, game <id>, totals',
             ['url', 'action', '[query...]'])
         self.addcommand(self.alias, 'alias',
             'Return the text for a restatsdb alias.', ["url"])
@@ -21,14 +21,34 @@ class Module(bot.Module):
 
     def main(self, context, args):
         action = args.getstr('action')
+        http = self.server.rget('http.url')
         actionurl = None
         if action == "toprace":
             actionurl = "/maps/%s" % args.getstr('query')
         elif action == "game":
             actionurl = "/games/%s" % args.getstr('query')
+        elif action == "totals":
+            try:
+                jgames = http.request(
+                    args.getstr('url') + '/get/games').json()
+                jservers = http.request(
+                    args.getstr('url') + '/get/servers').json()
+                jmaps = http.request(
+                    args.getstr('url') + '/get/maps').json()
+                jplayers = http.request(
+                    args.getstr('url') + '/get/players').json()
+            except http.Error:
+                return "Unable to contact API."
+            except ValueError:
+                return "Invalid API response."
+            return "Games: %d, Maps: %d, Servers: %d, Players: %d" % (
+                len(jgames),
+                len(jmaps),
+                len(jservers),
+                len(jplayers),
+                )
         else:
             return "Invalid action."
-        http = self.server.rget('http.url')
         try:
             json = http.request(args.getstr('url') + '/get' + actionurl).json()
         except http.Error:
