e20d98813d861115e823b0d41f9c41e909702b95
acz13 <alchzh@gmail.com>
2015-10-26 19:17:00 -0400

    safeshare

--- plugins/internet/__init__.py
+++ plugins/internet/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Internet utility commands.
 import bot
-from . import ip, url
-[bot.reload(x) for x in [ip, url]]
+from . import ip, url, safeshare
+[bot.reload(x) for x in [ip, url, safeshare]]
