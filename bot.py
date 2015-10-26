cdf320b2a7bcb83ae171354f6b928365f0ce58b1
Blank <shacknetisp@mail.com>
2015-10-26 19:02:31 -0400

    name in args, v0.2.11

--- bot.py
+++ bot.py
@@ -123,15 +123,20 @@ class Args:
 
     def __init__(self, d):
         self.d = d
+        self.defaults = {}
 
     def default(self, n, d):
         """Set the default value of arg <n> to <d>"""
-        if n not in self.d:
-            self.d[n] = d
+        self.defaults[n] = d
+
+    def __contains__(self, n):
+        return n in self.d
 
     def getstr(self, n):
         """Get the arg <n> as a str."""
         try:
+            if n not in self.d and n in self.defaults:
+                return str(self.defaults[n])
             return str(self.d[n])
         except KeyError:
             raise NoArg("Argument not found: %s" % n)
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import platform
-version = "0.2.10"
+version = "0.2.11"
 name = "Vepybot"
 namever = "%s %s" % (name, version)
 platformtuple = (name, version, "Python %s on %s (%s)" % (
