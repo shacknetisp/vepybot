2feec8275f9aefebbc2cbb40c92a6c9d21cfb4e8
Jay Fault <alchzh@gmail.com>
2015-10-26 14:41:22 +0000

    add piglatin, uses bpabel's code

--- plugins/misc/__init__.py
+++ plugins/misc/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Various utility commands.
 import bot
-from . import calc, chatbot, lookup
+from . import calc, chatbot, lookup, piglatin
 [bot.reload(x) for x in [calc, chatbot, lookup]]
