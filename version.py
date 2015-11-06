c061070a8dbd715cb596e667fe5572111c5591a0
Blank <shacknetisp@mail.com>
2015-11-05 22:23:49 -0500

    0.2.17

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import platform
-version = "0.2.16"
+version = "0.2.17"
 name = "Vepybot"
 namever = "%s %s" % (name, version)
 platformtuple = (name, version, "Python %s on %s (%s)" % (
