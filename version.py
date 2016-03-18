1d8ac305ea16d62974aacfa0906f9410454be876
Blank <shacknetisp@mail.com>
2016-03-17 21:30:17 -0400

    Less version information

--- version.py
+++ version.py
@@ -3,10 +3,9 @@ import platform
 version = "0.2.24"
 name = "Vepybot"
 namever = "%s %s" % (name, version)
-platformtuple = (name, version, "Python %s on %s (%s)" % (
+platformtuple = (name, version, "Python %s on %s" % (
     platform.python_version(),
-    platform.system(),
-    platform.release()
+    platform.system()
 ))
 url = "https://github.com/shacknetisp/vepybot"
 source = "https://github.com/shacknetisp/vepybot"
