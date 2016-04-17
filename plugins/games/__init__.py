b1c6b111abde837f639a04cb27093c4adbd10170
Beha <shacknetisp@mail.com>
2016-04-17 16:58:25 -0400

    Remove chatbot and piglatin

--- plugins/games/__init__.py
+++ plugins/games/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Games & Amusement
 import bot
-from . import piglatin, chatbot, fortune
-[bot.reload(x) for x in [piglatin, chatbot, fortune]]
+from . import fortune
+[bot.reload(x) for x in [fortune]]
